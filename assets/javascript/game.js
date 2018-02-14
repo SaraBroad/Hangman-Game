e1c5d252a25ad9fdcf98be8f4eb87f8fb0e9dd6c
Sara Broad <sarabroad@Saras-MacBook-Pro-2.local>
2018-02-14 18:20:30 -0500

    push for test

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,6 @@
 <h2>Press any key to get started</h2>
 <div id="chances">Chances</div>
 
-// create an object
 
 var photographers = ["steichen", "bresson", "eggleston", "evans", "arbus"];
 var win = 0;
@@ -10,31 +9,56 @@ var steichen = [" ", " ", " ", " ", " ", " ", " ", " "];
 
 document.onkeyup = function (event) {
     var userGuess = event.key;
+    console.log(userGuess);
     var computerGuess = photographers[Math.floor(Math.random()*photographers.length)];
-    alert("Let's begin");
+    console.log(computerGuess);
 }
 
 for (var i = 0; i<photographs.length; i++) {
 
-if photographers === "steichen" {
+if computerGuess === photographers[1] {
     if ((userGuess === s) || || (userGuess === e) || (userGuess === i) || (userGuess === c) || (userGuess === e) || (userGuess === n)) {
-        userGuess.push(steichen[0]);
+        userGuess.push(steichen[0]) && guesses--;
     } else if (userGuess === t){
-        userGuess.push(steichen[1]);
-    } else if (userGuess === e) {
-        userGuess.push(steichen[2][6]);
-    } else if (userGuess === i) {
-        userGuess.push(steichen[3]);
-    } else if (userGuess === c) {
-        userGuess.push(steichen[4]);
-    } else if (userGuess === h) {
-        userGuess.push(steichen[5]);
-    } else if (userGuess === n) {
-        userGuess.push(steichen[7]);
-     }
+        userGuess.push(steichen[1]); 
+        guesses--;
 
+    } else if (userGuess === e) {
+        userGuess.push(steichen[2][6]) && guesses--;
+    } else if (userGuess === i) {
+        userGuess.push(steichen[3]) && guesses--;
+    } else if (userGuess === c) {
+        userGuess.push(steichen[4]) && guesses--;
+    } else if (userGuess === h) {
+        userGuess.push(steichen[5]) && guesses--;
+    } else if (userGuess === n) {
+        userGuess.push(steichen[7]) && guesses--;
+     }
+    }
+}
         guesses-- && document.querySelector("chances").innerHTML = " ";
     }
 }
 
 }
+
+function consoleInside(arr) {
+    for (var i = 0; i<photographs.length; i++) {
+
+    }
+
+}
+
+<script type="text/javascript">
+// Let's start by grabbing a reference to the <span> below.
+var userText = document.getElementById("user-text");
+
+// Next, we give JavaScript a function to execute when onkeyup event fires.
+document.onkeyup = function(event) {
+  userText.textContent = event.key;
+};
+
+OR if (userGuess !== "s", "t", "e", "i", "c", "h", "n") {
+    alert("Wrong answer!");
+}
+    
